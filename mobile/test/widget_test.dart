8f37d0368eea578f138d039f2efe2b3506353b8b
Aravind Lal <aravindlal2006@gmail.com>
2026-07-31 18:21:34 +0530

    test(mobile): add widget_test smoke test

---
+++ mobile/test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Smoke test app launch', (WidgetTester tester) async {
+    // Basic test
+    expect(true, isTrue);
+  });
+}
